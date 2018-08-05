2680c84077c181c24208315fad27133622fdfdb7
Jamee Kim <jameekim@nodaplife.me>
2018-08-06 06:38:32 +0900

    added i18n korean files and finished with index and pages

---
+++ docs/app/i18n/ko-KR/components.js
@@ -0,0 +1,6 @@
+export default {
+  code: {
+    copyMessage: '복사되었습니다!',
+    copy: '복사'
+  }
+}
---
+++ docs/app/i18n/ko-KR/index.js
@@ -0,0 +1,16 @@
+import components from './components'
+import pages from './pages'
+
+export default {
+  aboutMaterial: 'Vue Material에 대하여',
+  madeWith: '사랑을 담아',
+  by: '만든 이',
+  code: '코드',
+  example: '예시',
+  default: '기본',
+  darkTheme: '어두운 테마',
+  lightTheme: '밝은 테마',
+
+  pages,
+  components
+}
